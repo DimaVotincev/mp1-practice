5a57ecf71017876b7bf54164aa9199fc08e91691
DimaVotincev <dvotinccev@gmail.com>
2023-09-22 13:45:12 +0300

    Initial structure

---
+++ My_first_solution/My_first_project/main.c
@@ -0,0 +1,15 @@
+
+
+
+#include <stdio.h>
+
+int main() {
+    int a;
+    int b;
+    
+    scanf_s("%d %d", &a, &b);
+    printf("The summ of two numbers = %d", a + b);
+    return 0;
+}
+
+// ctrl + shift + B   
