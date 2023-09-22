48d85c49931f6d05ad5ef6570fb4252469fb6548
DimaVotincev <dvotinccev@gmail.com>
2023-09-22 14:02:57 +0300

    Fix problem

--- My_first_solution/My_first_project/main.c
+++ My_first_solution/My_first_project/main.c
@@ -7,9 +7,19 @@ int main() {
     int a;
     int b;
     
-    scanf_s("%d %d", &a, &b);
+    scanf("%d %d", &a, &b);
     printf("The summ of two numbers = %d", a + b);
     return 0;
+
+    //adding a new item
+
+    int l1 = 10000;
 }
 
 // ctrl + shift + B   
+// ctrl + F5   (запуск без пошаговой отладки)
+// бинарные файлы не хранить на гитхабе
+
+
+// для работы с scanf нужно в св-вах проекта в C/C++ добавить _CRT_SECURE_NO_WARNINGS и разделить ;
+//  _CRT_SECURE_NO_WARNINGS;
