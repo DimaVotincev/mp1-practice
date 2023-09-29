0e667c4204d07eacd9d0dcf41cb6e1122a26efcd
DimaVotincev <dvotinccev@gmail.com>
2023-09-29 14:21:58 +0300

    Changes in class

--- solution_1/project_1/code1.c
+++ solution_1/project_1/code1.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
 #include <math.h>
+/*
+Test cases
 
+Test1
+input:
+0 0 10
+999 999 1
+output:
+
+Test2
+input:
+0 0 10
+11 0 1
+output:
+
+Test3
+input:
+0 0 10
+12 0 3
+output:
+
+Test4
+input:
+0 0 5
+1 1 10
+output:
+
+Test5
+input:
+0 0 5
+3 0 2
+output:
+
+
+*/
 int main() {
     double x1, y1, r1;
     double x2, y2, r2;
@@ -15,22 +49,31 @@ int main() {
         printf("Wrong radius input!");
         return 0;
     }
-
-    
-    if (x1 == x2 && y1 == y2) {
+    double a1 = (x2 - x1);
+    double a2 = (y2 - y1);
+    double distance = sqrt(a1 * a1 - a2 * a2);
+    double catch_error = 0.01;
+    if (abs((distance + r1) - r2) < catch_error || abs((distance + r2) - r1) < catch_error) {
+        printf("One circle inside other one and touch at one dot!");
+        
+        return 0;
+    }
+    else if ((x1 == x2 && y1 == y2) || abs(r1 - (distance+r2) < catch_error))  {
         printf("One circle inside other one!\n");
         return 0;
     }
+
     else {
-        double a1 = (x2 - x1);
-        double a2 = (y2 - y1);
-        double distance = sqrt(a1 * a1 - a2 * a2);
-        double catch_error = 0.01;
-        if (distance - (r1 + r2) < 0) {
+        
+        
+        
+        
+        
+        if ((distance - (r1 + r2)) < 0) {
             printf("Circles intersects at two points\n");
             return 0;
         }
-        else if (distance - (r1 + r2) < catch_error || distance - (r1 + r2) > catch_error) {
+        else if ((abs(distance - (r1 + r2)) < catch_error)) {
             printf("Circles touches\n");
             return 0;
         }
@@ -39,7 +82,7 @@ int main() {
             return 0;
         }
     }
-    
-    
-    
+
+
+
 }
