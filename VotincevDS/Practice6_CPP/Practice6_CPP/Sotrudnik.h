2692689857975e81bc45f8e5875ab4c10a126b90
DimaVotincev <dvotinccev@gmail.com>
2024-05-12 20:27:47 +0300

    5

--- VotincevDS/Practice6_CPP/Practice6_CPP/Sotrudnik.h
+++ VotincevDS/Practice6_CPP/Practice6_CPP/Sotrudnik.h
@@ -24,7 +24,7 @@ public:
 
     
     void print_dates(); // вывод даты назначения и поступления
-    void print_generalinfo();
+    void print_generalinfo(); // вывод общей информации
     int get_old() { return this->is_old; };
     Name& get_name() { return this->name; }
     Passport& get_pass() { return this->passport;  }
